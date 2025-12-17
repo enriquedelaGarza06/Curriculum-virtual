d0ec91012d9e4d879021aacd35153b98793575b6
enriquedelaGarza06 <enrique.pdlg02@gmail.com>
2025-12-16 21:52:40 -0600

    Implemented Backend POST route for contact form

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
-const express=require("express")
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3000;
 
-const app=express()
-const port=3000
-
-console.log("ejecutando aplicacion")
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get("/",(req,res)=>{
-    res.sendFile("./index.html",{root: __dirname })
-})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-app.get("/sobre_mi",(req,res)=>{
-    res.sendFile("./sobre_mi.html",{root: __dirname })
-})
+app.get('/sobre_mi', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sobre_mi.html'));
+});
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile("./status404.html",{root: __dirname })
-})
+app.post('/contacto', (req, res) => {
+    const { nombre, email, mensaje } = req.body;
+    
+    console.log(`📝 Nuevo mensaje recibido:`);
+    console.log(`Nombre: ${nombre}`);
+    console.log(`Email: ${email}`);
+    console.log(`Mensaje: ${mensaje}`);
 
-app.listen(port, ()=>{
-    console.log(`Aplicación corriendo ahora sí en el puerto ${port}`)
-})
+    res.send(`
+        <h1>Thank you, ${nombre}!</h1>
+        <p>I have received your message. I will contact you at ${email} soon.</p>
+        <a href="/">Go back home</a>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
