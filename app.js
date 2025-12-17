3af577683e94500255b0e313c6fb9a1d22265ca1
enriquedelaGarza06 <enrique.pdlg02@gmail.com>
2025-12-16 22:06:08 -0600

    Created API endpoint to serve projects data

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
+const projectsData = require('./data/projects.json');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -18,17 +19,12 @@ app.get('/sobre_mi', (req, res) => {
 
 app.post('/contacto', (req, res) => {
     const { nombre, email, mensaje } = req.body;
-    
-    console.log(`📝 Nuevo mensaje recibido:`);
-    console.log(`Nombre: ${nombre}`);
-    console.log(`Email: ${email}`);
-    console.log(`Mensaje: ${mensaje}`);
+    console.log(`📝 Mensaje de: ${nombre} (${email})`);
+    res.send(`<h1>Thanks ${nombre}! Message received.</h1><a href="/">Go back</a>`);
+});
 
-    res.send(`
-        <h1>Thank you, ${nombre}!</h1>
-        <p>I have received your message. I will contact you at ${email} soon.</p>
-        <a href="/">Go back home</a>
-    `);
+app.get('/api/projects', (req, res) => {
+    res.json(projectsData);
 });
 
 app.listen(port, () => {
